77e9d4d3ef5768b01a7199d6d6eabdab86487dbd
maks <maxim_86nva@mail.ru>
2023-04-15 10:54:43 +0300

    созданы классы Author и Book с конструкторами, геттерами и сеттерами

--- src/com/skypro/Main.java
+++ src/com/skypro/Main.java
@@ -3,6 +3,28 @@ package com.skypro;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Author lermontov = new Author("Mikhail", "Lermontov");
+        Author pushkin = new Author("Alexander", "Pushkin");
+
+        Book geroyNashegoVremeni = new Book("Geroy nashego vremeni", lermontov, 2020);
+        Book ruslanILydmila = new Book("Ruslan i Lydmila", pushkin, 2001);
+
+        System.out.println("geroyNashegoVremeni:");
+        System.out.println(geroyNashegoVremeni.getName());
+        System.out.println(geroyNashegoVremeni.getAuthor().getName() + " " + geroyNashegoVremeni.getAuthor().getSurname());
+        System.out.println(geroyNashegoVremeni.getYear());
+
+        System.out.println();
+
+        System.out.println("ruslanILydmila:");
+        System.out.println(ruslanILydmila.getName());
+        System.out.println(ruslanILydmila.getAuthor().getName() + " " + ruslanILydmila.getAuthor().getSurname());
+        System.out.println(ruslanILydmila.getYear());
+
+        System.out.println();
+
+        geroyNashegoVremeni.setYear(2007);
+        System.out.println("год издания geroyNashegoVremeni " + geroyNashegoVremeni.getYear());
     }
 }
